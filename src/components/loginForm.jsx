5d76791aeff01723b661ecc9c5d1989f8ec38854
Youssef Benali <youssef.benali294@gmail.com>
2022-06-02 20:23:11 +0200

    login and register service added

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "../common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   // ! A state can't be null or undefined
@@ -22,8 +23,19 @@ class LoginForm extends Form {
 
   // errors['username'] (if it was an array, find() method...)
 
-  doSubmit = () => {
-    // Call the server
+  doSubmit = async () => {
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem("token", jwt);
+      this.props.history.push("/");
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
