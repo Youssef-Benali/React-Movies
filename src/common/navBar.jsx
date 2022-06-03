fc81bd7844e6e6d3f10c996c54f554043524cc92
Youssef Benali <youssef.benali294@gmail.com>
2022-06-03 06:24:21 +0200

    Logging out a user

--- src/common/navBar.jsx
+++ src/common/navBar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ user }) => {
             </React.Fragment>
           )}
 
-{user && (
+          {user && (
             <React.Fragment>
               <NavLink to="/profile" className="nav-item nav-link">
                 {user.name}
