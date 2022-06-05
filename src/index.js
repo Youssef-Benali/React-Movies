e6a5a3695d1a3d6838ca89f20bbf69ad882b0ded
Youssef Benali <youssef.benali294@gmail.com>
2022-06-05 13:07:44 +0200

    .env file added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import Movies from "./components/movies";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
 
 ReactDOM.render(
   <BrowserRouter>
